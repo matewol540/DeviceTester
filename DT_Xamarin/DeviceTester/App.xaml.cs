473b3adb2b2ae23739275663c1b1042c13791ccb
Mateusz Wolski <wolski.mateusz113@gmail.com>
2021-06-26 18:35:24 +0200

    Initital appliation

---
+++ DT_Xamarin/DeviceTester/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DeviceTester.Content.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeviceTester
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
