513f4fb8946e2d24281429973d0b341a254af904
Mateusz Wolski <wolski.mateusz113@gmail.com>
2021-11-12 00:08:57 +0100

    Refactor gps to track user location over time and in background mode, additional pages templates

---
+++ DT_Xamarin/DeviceTester/Content/Pages/Pheripheries/BluetoothPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DeviceTester.Content.Pages.Pheripheries
+{
+    public partial class BluetoothPage : ContentPage
+    {
+        public BluetoothPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DT_Xamarin/DeviceTester/Content/Pages/System/DeviceInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DeviceTester.Content.Pages.System
+{
+    public partial class DeviceInfoPage : ContentPage
+    {
+        public DeviceInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
