00d90057c2384a817ce49491442115852f7d21e1
Mateusz Wolski <wolski.mateusz113@gmail.com>
2021-11-13 01:49:53 +0100

    Searching bluetooth devices implemented

--- DT_Xamarin/DeviceTester/Content/Pages/Pheripheries/BluetoothPage.xaml.cs
+++ DT_Xamarin/DeviceTester/Content/Pages/Pheripheries/BluetoothPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using DeviceTester.Content.Views;
 using DeviceTester.Interfaces;
 using DeviceTester.Models;
 using DeviceTester.Resources;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
 using Xamarin.Forms;
 
 namespace DeviceTester.Content.Pages.Pheripheries
@@ -13,6 +15,9 @@ namespace DeviceTester.Content.Pages.Pheripheries
     {
         private Animation _animation;
         ObservableCollection<BluetoothDeviceModel> models = new ObservableCollection<BluetoothDeviceModel>();
+
+        IBluetoothLE ble;
+        IAdapter adapter;
 
 
         public BluetoothPage()
@@ -25,8 +30,62 @@ namespace DeviceTester.Content.Pages.Pheripheries
             tmpComp.LineraGradientBck.GradientStops[0].Color = tempTuple.Item2;
             tmpComp.LineraGradientBck.GradientStops[1].Color = tempTuple.Item3;
             this.MainGrid.Children.Add(tmpComp, 0, 0);
-            this.DeviceListView.ItemsSource = models;
+
+
+            this.ble = CrossBluetoothLE.Current;
+            this.adapter = CrossBluetoothLE.Current.Adapter;
+            //this.DeviceListView.ItemsSource = models;
+
+            AddBluetoothEventHandlers();
         }
+
+        private void AddBluetoothEventHandlers()
+        {
+            ble.StateChanged += (sender, args) =>
+            {
+                if (args.NewState == BluetoothState.On)
+                {
+                    DeviceListView.IsEnabled = true;
+                    DeviceListView.IsPullToRefreshEnabled = true;
+                    DeviceListView.RefreshCommand = new Command(() => {
+                        DeviceListView.IsRefreshing = true;
+                        ScanForDevicesAsync();
+                        DeviceListView.IsRefreshing = false;
+                        });
+                }
+                else if (args.OldState == BluetoothState.On)
+                {
+                    models.Clear();
+                    DeviceListView.IsEnabled = false;
+                    DeviceListView.IsPullToRefreshEnabled = false;
+                    DeviceListView.RefreshCommand = null;
+                }
+            };
+            adapter.DeviceDiscovered += (sender, args) =>
+            {
+                var tempDevice = args.Device;
+                var tempModel = new BluetoothDeviceModel
+                {
+                    DeviceName = tempDevice.Name,
+                    DeviceId = tempDevice.Id,
+                    RSSI = tempDevice.Rssi,
+                    State = tempDevice.State
+                };
+                models.Add(tempModel);
+            };
+        }
+
+        private async void ScanForDevicesAsync()
+        {
+            if (adapter.IsScanning == false)
+                await adapter.StartScanningForDevicesAsync();
+
+        }
+
+        
+        
+
+
 
         public void ChangeDescriptionState(bool State)
         {
@@ -44,7 +103,15 @@ namespace DeviceTester.Content.Pages.Pheripheries
                         (d) => MainGrid.RowDefinitions[0] = new RowDefinition() { Height = HeightValue });
             _animation.Commit(this, "GPS Animation", 16, 1000000, Easing.BounceIn, null, null);
         }
+
+        void DeviceListView_Refreshing(System.Object sender, System.EventArgs e)
+        {
+            
+        }
     }
+
+
+
 
     public class BluetoothPageFactory : PageFactory
     {
--- DT_Xamarin/DeviceTester/Models/BluetoothDeviceModel.cs
+++ DT_Xamarin/DeviceTester/Models/BluetoothDeviceModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using Plugin.BLE.Abstractions;
+using Plugin.BluetoothLE;
+
 namespace DeviceTester.Models
 {
     public class BluetoothDeviceModel
     {
-        public BluetoothDeviceModel()
-        {
-        }
+        public string DeviceName { get; set; }
+        public Guid DeviceId { get; set; }
+        public int RSSI { get; set; }
+        public DeviceState State { get; set; }
     }
 }
